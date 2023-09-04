5e317cdce3faeaea3a409665858a1d129ad61ec0
Amrutha R Nair <rnairamrutha67@gmail.com>
2023-09-04 23:45:28 +0530

    commit

---
+++ webapp/resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+@section('title','contact')
+@section('content')
+<div class="container">
+    <h1>Contact</h1><br/>
+    
---
+++ webapp/resources/views/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title','default')</title>
+</head>
+<body>
+    <nav class="nav">
+        <ul>
+            <li class="menu"><a href="{{url('/')}}">HOME</a></li>
+            <li class="menu"><a href="{{route('about')}}">ABOUT</a></li>
+            <li class="menu"><a href="{{route('contact')}}">CONTACT</a></li>
+        </ul>
+    </nav>
+    @yield('main content')
+</body>
+</html>
