b48d111e3a22b3a5f0cc8cfd683ea135c10f4f80
Amrutha R Nair <rnairamrutha67@gmail.com>
2023-09-13 04:21:43 +0530

    commit

--- webapp/resources/views/about.blade.php
+++ webapp/resources/views/about.blade.php
@@ -12,7 +12,7 @@
   </head>
   <body>
     <div class="container">
-      <h1 class="mb-5">Test Validation Form</h1>
+      <h1 class="mb-5">Test_Validation Form</h1>
 
       <form>
         <div class="mb-3">
