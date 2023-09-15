1c852ddb5ea3aabbf5e21768710611bcc83fb900
Amrutha R Nair <rnairamrutha67@gmail.com>
2023-09-15 21:44:40 +0530

    cmt

--- webapp/resources/views/about.blade.php
+++ webapp/resources/views/about.blade.php
@@ -16,7 +16,7 @@
 
       <form>
         <div class="mb-3">
-          <label for="username" class="form-label">Username</label>
+          <label for="username" class="form-label">User Name</label>
           <input type="text" class="form-control" id="username" required />
         </div>
         <div class="mb-3">
