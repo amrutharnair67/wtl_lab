be3ff0291fe202b0d8ef569849fee33f12168310
Amrutha R Nair <rnairamrutha67@gmail.com>
2023-09-16 22:04:53 +0530

    cmt

--- webapp/resources/views/about.blade.php
+++ webapp/resources/views/about.blade.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Bootstrap Form Validation</title>
+    <title>Bootstrap_Form_Validation</title>
   </head>
   <body>
     <div class="container">
